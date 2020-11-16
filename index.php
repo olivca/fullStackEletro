945a62dfb078271b337d673f91031fde51d646fc
olivca <carolineol.jw@gmail.com>
2020-11-15 23:52:28 -0300

    arrumei index

--- index.php
+++ index.php
@@ -10,12 +10,17 @@
     integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 </head>
 <body>
-   <nav id="menu">
-        <a href="index.php"><img src="imagens/logo.png" width="150px" height="60px"></a>
-        <a href="./php/produtos.php" id="prod" onmousemove="teste(this)">Nossos produtos</a>
-        <a href="./php/loja.php" id="loj" onmousemove="teste2(this)">Nossas lojas</a>    
-        <a href="./php/contato.php" id="cont" onmousemove="teste3(this)">Fale conosco</a>    
-    </nav> 
+
+<nav class="navbar navbar-expand-lg" style="background-color:black">
+        <div class="container col-lg-12 justify-content-between" >
+        <a class="navbar-brand" href="./index.php"><img src="./imagens/logo.png" width="250px" height="60px"></a>
+        
+        <a class="py-2 d-none d-md-inline-block" href="./php/produtos.php" id="prod" onmousemove="teste(this)">Nossos produtos</a>
+        <a class="py-2 d-none d-md-inline-block" href="./php/loja.php" id="loj" onmousemove="teste2(this)">Nossas lojas</a>    
+        <a class="py-2 d-none d-md-inline-block" href="./php/contato.php" id="cont" onmousemove="teste3(this)">Fale conosco</a>
+        <a class="py-2 d-none d-md-inline-block" href="./php/pedidos.php" id="cont" onmousemove="teste3(this)">Realizar Pedido</a> 
+      </div>
+</nav>
 
     <br><br>
     <main>
