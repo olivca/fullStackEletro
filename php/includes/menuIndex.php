a4f09151be13d3e06249e80fb5f879fc2f8289ef
olivca <carolineol.jw@gmail.com>
2020-12-02 20:56:09 -0300

    organização do código

--- php/includes/menuIndex.php
+++ php/includes/menuIndex.php
@@ -6,9 +6,9 @@
         <div class="container col-lg-12 justify-content-between" >
         <a class="navbar-brand" href="../index.php"><img src="imagens/logo.png" width="250px" height="60px"></a>
         
-        <a class="py-2 d-none d-md-inline-block" href="../php/produtos.php" id="prod">Nossos produtos</a>
-        <a class="py-2 d-none d-md-inline-block" href="../php/loja.php" id="loj">Nossas lojas</a>    
-        <a class="py-2 d-none d-md-inline-block" href="../php/contato.php" id="cont">Fale conosco</a>
-        <a class="py-2 d-none d-md-inline-block" href="../php/pedidos.php" id="cont">Realizar Pedido</a> 
+        <a class="py-2 d-none d-md-inline-block" href="php/produtos.php" id="prod">Nossos produtos</a>
+        <a class="py-2 d-none d-md-inline-block" href="php/loja.php" id="loj">Nossas lojas</a>    
+        <a class="py-2 d-none d-md-inline-block" href="php/contato.php" id="cont">Fale conosco</a>
+        <a class="py-2 d-none d-md-inline-block" href="php/pedidos.php" id="cont">Realizar Pedido</a> 
       </div>
 </nav>
