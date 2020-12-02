f9d09c401e1dec5017ab9a68397fd6e2c2577d6a
olivca <carolineol.jw@gmail.com>
2020-12-02 20:46:59 -0300

    organização do código

--- index.php
+++ index.php
@@ -11,18 +11,12 @@
 </head>
 <body>
 
-<nav class="navbar navbar-expand-lg" style="background-color:black">
-        <div class="container col-lg-12 justify-content-between" >
-        <a class="navbar-brand" href="./index.php"><img src="./imagens/logo.png" width="250px" height="60px"></a>
-        
-        <a class="py-2 d-none d-md-inline-block" href="./php/produtos.php" id="prod" onmousemove="teste(this)">Nossos produtos</a>
-        <a class="py-2 d-none d-md-inline-block" href="./php/loja.php" id="loj" onmousemove="teste2(this)">Nossas lojas</a>    
-        <a class="py-2 d-none d-md-inline-block" href="./php/contato.php" id="cont" onmousemove="teste3(this)">Fale conosco</a>
-        <a class="py-2 d-none d-md-inline-block" href="./php/pedidos.php" id="cont" onmousemove="teste3(this)">Realizar Pedido</a> 
-      </div>
-</nav>
+    <?php 
+    include_once "php/includes/cabecalho.php";
+    
+    include_once "php/includes/menuIndex.php";
+    ?> 
 
-    <br><br>
     <main>
     <div class="conteiner">
     <h1> Seja Bem Vindo</h1>
@@ -32,19 +26,13 @@
     </div> 
     </main> 
     <hr>
-    <br><br>
-    <br><br>
-    <br><br>
-    <br><br>
 
 
     <footer> 
-        <p id="copy">&copy; Recode Pro</p>
+    <?php
+        include_once "php/includes/rodape.php";
+    ?>
     </footer>
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" 
-    crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" 
-    crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.
+    
 </body>
 </html>
---
+++ php/includes/menuIndex.php
@@ -0,0 +1,14 @@
+<link rel="stylesheet" href="../css/style.css">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" 
+    integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+<nav class="navbar navbar-expand-lg" style="background-color:black">
+        <div class="container col-lg-12 justify-content-between" >
+        <a class="navbar-brand" href="../index.php"><img src="imagens/logo.png" width="250px" height="60px"></a>
+        
+        <a class="py-2 d-none d-md-inline-block" href="../php/produtos.php" id="prod">Nossos produtos</a>
+        <a class="py-2 d-none d-md-inline-block" href="../php/loja.php" id="loj">Nossas lojas</a>    
+        <a class="py-2 d-none d-md-inline-block" href="../php/contato.php" id="cont">Fale conosco</a>
+        <a class="py-2 d-none d-md-inline-block" href="../php/pedidos.php" id="cont">Realizar Pedido</a> 
+      </div>
+</nav>
